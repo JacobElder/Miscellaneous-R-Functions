fae9e5a6be0205059f053a3862875af3db290a20
JacobElder <jacobelder52@gmail.com>
2022-05-19 12:34:56 -0700

    Update assortativityNA.R

--- assortativityNA.R
+++ assortativityNA.R
@@ -1,6 +1,5 @@
-library(igraph)
 assortativityNA <- function(graph, types1, types2 = NULL, directed = TRUE, na.rm = TRUE){
-  
+  library(igraph)
   if(na.rm==TRUE){
     graph <- delete_vertices(graph, which(is.na(types1)))
     types1 <- types1[!is.na(types1)]
