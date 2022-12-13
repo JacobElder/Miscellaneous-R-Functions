e9afecaee046bc4a65b67c810609102c78a9c6aa
JacobElder <jacobelder52@gmail.com>
2022-12-13 13:23:11 -0800

    effects coding

--- named.effects.ref.R
+++ named.effects.ref.R
@@ -1,4 +1,6 @@
 named.contr.sum<-function(x, ...) {
+  # Credit to StackOverflow for this function https://stackoverflow.com/questions/24515892/r-how-to-contrast-code-factors-and-retain-meaningful-labels-in-output-summary
+  
   if (is.factor(x)) {
     x <- levels(x)
   } else if (is.numeric(x) & length(x)==1L) {
@@ -6,7 +8,8 @@ named.contr.sum<-function(x, ...) {
   }
   x<-contr.sum(x, ...)
   colnames(x) <- apply(x,2,function(x) 
-    paste(names(x[x>0]), names(x[x<0]), sep="-")
+    #paste(names(x[x>0]), names(x[x<0]), sep="-")
+    paste(names(x[x>0]))
   )
   x
 }
