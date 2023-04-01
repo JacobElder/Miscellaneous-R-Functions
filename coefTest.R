33e2b339ca80f256d34c80b8f68c8f963afe8397
JacobElder <jacobelder52@gmail.com>
2023-03-31 18:16:39 -0700

    coefTest

---
+++ coefTest.R
@@ -0,0 +1,13 @@
+# Clogg, C. C., Petkova, E., & Haritou, A. (1995). Statistical methods for comparing regression coefficients between models. American Journal of Sociology, 100(5), 1261-1293. 
+
+pTest <- function(Z){
+  p <- 2*pnorm(-abs(Z))
+  return(p)
+}
+
+zTest <- function(b1,b2,se1,se2){
+  Z<-(b1 - b2)/sqrt( ((se1)^2) + ((se2)^2) )
+  return(list("Z"=Z,"p"=p))
+}
+
+
